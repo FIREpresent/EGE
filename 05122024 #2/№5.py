bbb49b6892f7becd823b924b25f16aa40b6b8f70
FIREpresent <misha.boris07@gmail.com>
2024-12-12 20:34:57 +0300

    мини-апдейт

--- 05122024 #2/№5.py
+++ 05122024 #2/№5.py
@@ -5,6 +5,6 @@ for num in range(10_000, 1000, -1):
     sum2 = v[1] + v[2]
     sum3 = v[2] + v[3]
     summ = sum1 + sum2 + sum3
-    if str(summ - max(sum1, sum2, sum3) - min(sum1, sum2, sum3)) + str(max(sum1, sum2, sum3)) == '1315':
+    if str(summ - max(sum1, sum2, sum3) - min(sum1, sum2, sum3)) + str(max(sum1, sum2, sum3)) == '1215':
         print(num)
         break
